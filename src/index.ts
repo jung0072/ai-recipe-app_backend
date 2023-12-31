517c7e89544ba8c8c550e28d817319ded981814d
jung0072 <jung0072@algonquinlive.com>
2023-12-31 08:25:19 -0500

    change cors origin

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,17 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: ["http://localhost:3000"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: ["http://localhost:3000"],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   optionsSuccessStatus: 200,
+// };
 const corsOptions2 = {
   origin: ["https://recipeasy-v1.vercel.app/"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cors(corsOptions2));
 
 app.use("/api/recipe/generate", openaiRecipeGenerateRoute);
